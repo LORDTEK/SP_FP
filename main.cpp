aa32f40e92db274917028965cc0278a3e0262e2d
LORDTEK <LORDTEK@users.noreply.github.com>
2019-10-21 22:11:14 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "mbed.h"
+#include "SP_FP.h"
+
+Serial pc(USBTX, USBRX);  // TX, RX
+SP_FP sp_fp;
+
+int main() {
+    pc.baud(9600);
+    printf("uint32_t=0x41460000 \t float=%f\n", sp_fp.decoder(0x41460000));
+}
